96290d90f1388138ec06a54a5fa2595854c3ce69
Robin Rumpunen <robinrumpunen@gmail.com>
2019-09-19 20:59:59 +0200

    fixed modeSwitcher styling, placement & responsive placement.

--- assets/script/modeSwitcher.js
+++ assets/script/modeSwitcher.js
@@ -1,17 +1,17 @@
 const userPrefers = getComputedStyle(document.documentElement).getPropertyValue('content');	
 
 if (theme === "dark") {
-	document.getElementById("theme-toggle").innerHTML = "Light Mode";
+	document.getElementById("theme-toggle").innerHTML = "Light";
 } else if (theme === "light") {
-	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+	document.getElementById("theme-toggle").innerHTML = "Dark";
 } else if  (userPrefers === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	window.localStorage.setItem('theme', 'dark');
-	document.getElementById("theme-toggle").innerHTML = "Light Mode";
+	document.getElementById("theme-toggle").innerHTML = "Light";
 } else {
 	document.documentElement.setAttribute('data-theme', 'light');
 	window.localStorage.setItem('theme', 'light');
-	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+	document.getElementById("theme-toggle").innerHTML = "Dark";
 }
 
 function modeSwitcher() {
@@ -19,10 +19,10 @@ function modeSwitcher() {
 	if (currentMode === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		window.localStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+		document.getElementById("theme-toggle").innerHTML = "Dark";
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		window.localStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = "Light Mode";
+		document.getElementById("theme-toggle").innerHTML = "Light";
 	}
 }
--- engine.md
+++ engine.md
@@ -11,9 +11,9 @@ Down below you will see the current achievements of the project.
 Try it out for yourself!
 Download binaries here:
 <br>
-<a class="btn_dwnload" href="../download/XXX">Win32</a>
-<a class="btn_dwnload" href="../download/XXX">OSX  </a>
-<a class="btn_dwnload" href="../download/XXX">Linux</a>
+<a class="robinBtn robinBtnDwnload" href="../download/XXX">Win32</a>
+<a class="robinBtn robinBtnDwnload" href="../download/XXX">OSX  </a>
+<a class="robinBtn robinBtnDwnload" href="../download/XXX">Linux</a>
 
 ### Echo engine is a personal engine created from scratch* by me.
 It was started as a way for me to better understand what is truly going on
